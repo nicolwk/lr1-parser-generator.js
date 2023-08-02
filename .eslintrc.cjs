f747b32c00c997b45a197c1d6e19bfbc0af113f9
Nicol Leung <lwknico@gmail.com>
2023-08-02 23:26:26 +0800

    eslint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  ignorePatterns: ['dist/**/*.ts', 'dist/**/*.js'],
+  rules: {
+    'semi': ['error', 'always'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types'      : 'off',
+    '@typescript-eslint/no-unused-vars' : 'warn'
+  }
+};
